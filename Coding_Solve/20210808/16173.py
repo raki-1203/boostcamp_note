0a351caf79f16bcef105e3cca6a3961d32124bb8
raki <jfhdzzang@gmail.com>
2021-08-09 00:03:27 +0900

    백준 16173 문제 풀이

--- Coding_Solve/20210808/16173.py
+++ Coding_Solve/20210808/16173.py
@@ -2,20 +2,25 @@ from collections import deque
 from sys import stdin
 input = stdin.readline
 
-def bfs(graph, start):
-    visited = []
-    queue = deque([start])
+def bfs(graph):
+    visited = [[0] * N for _ in range(N)]
+    D = [(0, 1), (1, 0)]
+    queue = deque([(0, 0)])
+    result = 'Hing'
 
     while queue:
-        n = queue.popleft()
-        if n not in visited:
-            visited.append(n)
-            if n in graph:
-                temp = list(set(graph[n]) - set(visited))
-                queue += temp
-
-    return len(visited) - 1
-
+        x, y = queue.popleft()
+        for d in D:
+            nx = x + d[0] * graph[x][y]
+            ny = y + d[1] * graph[x][y]
+            if nx >= N or ny >= N:
+                continue
+            if graph[nx][ny] == -1:
+                return 'HaruHaru'
+            if visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+                queue.append((nx, ny))
+    return result
 
 if __name__ == "__main__":
     N = int(input())

f7e90497d09fa98f03840956f3d20e07d372f241
raki-1203 <jfhdzzang@gmail.com>
2021-08-23 08:54:37 +0900

    2636. 치즈 + 1987. 알파벳

---
+++ Coding_Solve/20210822/3109(풀이 진행중).py
@@ -0,0 +1,19 @@
+from collections import deque
+from sys import stdin
+input = stdin.readline
+
+def solution():
+    pass
+        
+
+if __name__ == '__main__':
+    # row, col
+    r, c = map(int, input().split())
+    # cheese 위치
+    cheese_arr = [list(map(int, input().split())) for _ in range(r)]
+    # 남아있는 치즈 개수 리스트
+    
+
+    
+
+    
